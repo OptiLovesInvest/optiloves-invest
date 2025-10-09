8eb1b98f64490eef1a57080202147f3109b4e6d0
Ruben Malungu <rubenmalungu@googlemail.com>
2025-10-09 18:47:52 +0100

    Revert "feat(thank-you): show order id when present"
    This reverts commit b80032b20a2d948ffd611d9eaa412d6bd99f187c.

--- frontend/src/app/thank-you/page.tsx
+++ frontend/src/app/thank-you/page.tsx
@@ -1,9 +1,8 @@
 ﻿export default function Page(){
   return (
     <main style={{padding:24}}>
-      <h1>Thank you</h1>{typeof window!=="undefined" && new URLSearchParams(window.location.search).get("oid") ? <p>Order: {new URLSearchParams(window.location.search).get("oid")}</p> : null}
+      <h1>Thank you</h1>
       <p>Your request was received. We’ll be in touch shortly.</p>
     </main>
   );
 }
-
