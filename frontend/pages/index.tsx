1b20c412928a370c210eb39ad8c088f5ff7a198d
Ruben Malungu <rubenmalungu@googlemail.com>
2025-10-23 14:56:03 +0100

    fix(build): remove pages/index.tsx to use App Router root

--- frontend/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-﻿import type { GetServerSideProps } from "next";
-export const getServerSideProps: GetServerSideProps = async () => {
-  return { redirect: { destination: "/thank-you", permanent: false } };
-};
-export default function Index(){ return null; }
