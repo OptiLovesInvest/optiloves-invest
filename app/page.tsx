bdb2c5a170fbfcf390d765100a81d2139ddb3523
Ruben Malungu <rubenmalungu@googlemail.com>
2026-03-09 23:20:27 +0000

    chore(ui): remove root app test files

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function Home() {
-  return (
-    <main style={{ padding: 24, background: "yellow", color: "black" }}>
-      <h1>ROOT APP TEST MARKER</h1>
-      <p>If you can see this, Vercel is serving the repo root app.</p>
-    </main>
-  );
-}
